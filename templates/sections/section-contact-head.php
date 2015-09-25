bdaa6f1cb01357b7f659bb53aa627b7f3c40ed5e
Champ Camba <champsupertramp@gmail.com>
2015-09-26 01:35:49 +0800

    update contact header

--- templates/sections/section-contact-head.php
+++ templates/sections/section-contact-head.php
@@ -7,13 +7,36 @@
         <p class="ns-text"><a class="ns-link" href="#" onclick="toggle_visibility('dNewsletter');">Newsletter Sign Up&nbsp;&nbsp;&nbsp;<img
                 class="ns-arrow" src="<?php echo $wd_wt->tpl_url['assets'];?>img/newsletter-arrow.png"></a>
         </p>
-        <ul class="menu-top">
+<!--         <ul class="menu-top">
             <li class="menu-top-item"><a class="menu-top-link" href="">Services</a></li>
             <li class="menu-top-item"><a class="menu-top-link" href="">Our Work</a></li>
             <li class="menu-top-item"><a class="menu-top-link" href="">Leadership</a></li>
             <li class="menu-top-item"><a class="menu-top-link" href="">Insights</a></li>
             <li class="menu-top-item"><a class="menu-top-link" href="">Contact Us</a></li>
         </ul>
+ -->
+        <?php
+        $defaults = array(
+          'theme_location'  => 'primary_navigation',
+          'menu'            => '',
+          'container_class' => '',
+          'container_id'    => '',
+          'menu_class'      => 'menu-top',
+          'menu_id'         => '',
+          'echo'            => true,
+          'fallback_cb'     => 'wp_page_menu',
+          'before'          => '',
+          'after'           => '',
+          'link_before'     => '',
+          'link_after'      => '',
+          'items_wrap'      => '<ul id="%1$s" class="%2$s menu-top-item">%3$s</ul>',
+          'depth'           => 0,
+          'walker'          => ''
+        );
+
+
+          wp_nav_menu( $defaults );
+        ?>
     </div>
     <div class="soc-wrap blue-gradient">
         <ul class="soc-menu">
