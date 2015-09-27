2ffb9459d0adb3eaf07c2b00e3edf977f26f4d66
Champ Camba <champsupertramp@gmail.com>
2015-09-28 06:29:44 +0800

    Add custom post type options settings

--- cs-framework-override/config/metabox.config.php
+++ cs-framework-override/config/metabox.config.php
@@ -8,8 +8,17 @@ if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
  * @version 1.0
  *
  */
+
+/**
+ * options fields settings
+ * @var array
+ */
 $metaboxes        = array();
 
+
+/**
+ * Case study post type
+ */
 $metaboxes[]      = array(
   'id'            => '_custom_meta_options',
   'title'         => 'Post Options',
@@ -31,6 +40,39 @@ $metaboxes[]      = array(
           'type'  => 'switcher',
           'title' => 'Show the link',
           'default' => true,
+        ),
+
+
+      ),
+    ),
+
+
+  ),
+);
+
+/**
+ * Single post
+ */
+$metaboxes[]      = array(
+  'id'            => '_custom_meta_options',
+  'title'         => 'Post Options',
+  'post_type'     => 'post', // or post or CPT
+  'context'       => 'normal',
+  'priority'      => 'default',
+  'sections'      => array(
+
+    // begin section
+    array(
+      'name'      => 'section_1',
+      'title'     => 'Post section',
+      'icon'      => 'fa fa-wifi',
+      'fields'    => array(
+
+        // a field
+        array(
+          'id'    => 'wd_blog_second_heading',
+          'type'  => 'wysiwyg',
+          'title' => 'Second Post Heading',
         ),
 
 
