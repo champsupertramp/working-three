bab533eee1a7505c2bcd34e6bebfe73d859692f1
Champ Camba <champsupertramp@gmail.com>
2015-09-27 23:25:52 +0800

    Add custom css and scripts settings

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+<?php global $wd_wt; ?>
 </body>
+<script type="text/javascript">
+<?php echo cs_get_option('wd_custom_scripts'); ?>
+</script>
+<style type="text/css">
+<?php echo cs_get_option('wd_custom_css'); ?>
+</style>
 <?php wp_footer(); ?>
 </html>
