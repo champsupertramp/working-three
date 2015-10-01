0c8a283c65f247a86d194e0b52f931eab11cd568
Champ Camba <champsupertramp@gmail.com>
2015-10-01 15:37:05 +0800

    Add body class

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ global $wd_wt;
 <head>
 <?php get_template_part('templates/sections/section', 'head'); ?>
 </head>
-<body>
+<body <?php body_class( ); ?>>
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
     your browser</a> to improve your experience.</p>
